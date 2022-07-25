665d705418f954e7e1873d080cca5217eb53bd04
zBiggui <bigguisenna@gmail.com>
2022-07-25 01:18:23 -0300

    test: add forecast test (with adjusts in global and jest-setup)

---
+++ test/jest-setup.ts
@@ -0,0 +1,9 @@
+import {SetupServer} from "@src/server";
+import supertest from 'supertest';
+
+// First test started
+beforeAll(() => {
+    const server = new SetupServer();
+    server.init();
+    global.testRequest = supertest(server.getApp());
+});
