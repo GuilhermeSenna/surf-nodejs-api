66ba6ce378e47035b3f5e5205de27c97cd0d96d3
zBiggui <bigguisenna@gmail.com>
2022-07-26 00:21:19 -0300

    feat: add stormGlass normalization and validation

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,5 +1,35 @@
 import { AxiosStatic } from "axios";
 
+export interface StormGlassPointSource {
+    [key: string]: number;
+}
+
+export interface StormGlassPoint {
+    time: string;
+    readonly waveHeight: StormGlassPointSource;
+    readonly waveDirection: StormGlassPointSource;
+    readonly swellDirection: StormGlassPointSource;
+    readonly swellHeight: StormGlassPointSource;
+    readonly swellPeriod: StormGlassPointSource;
+    readonly windDirection: StormGlassPointSource;
+    readonly windSpeed: StormGlassPointSource;
+}
+
+export interface StormGlassForecastResponse {
+    hours: StormGlassPoint[];
+}
+
+export interface ForecastPoint {
+    time: string;
+    waveHeight: number;
+    waveDirection: number;
+    swellDirection: number;
+    swellHeight: number;
+    swellPeriod: number;
+    windDirection: number;
+    windSpeed: number;
+}
+
 export class StormGlass {
     readonly stormGlassAPIParams =
         'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
@@ -7,8 +37,8 @@ export class StormGlass {
 
     constructor(protected request: AxiosStatic) { }
 
-    public async fetchPoints(lat: number, lng: number): Promise<{}> {
-        return this.request.get(
+    public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
+        const response = await this.request.get<StormGlassForecastResponse>(
             `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`,
             {
                 headers: {
@@ -16,7 +46,39 @@ export class StormGlass {
                 },
             }
         );
-        // return this.normalizeResponse(response.data);
+        return this.normalizeResponse(response.data);
+    }
+
+
+    private normalizeResponse(points: StormGlassForecastResponse): ForecastPoint[] {
+        // Validation before map items
+        return points.hours.filter(this.isValidPoint.bind(this)).map(point => ({
+            swellDirection: point.swellDirection[this.stormGlassAPISource],
+            swellHeight: point.swellHeight[this.stormGlassAPISource],
+            swellPeriod: point.swellPeriod[this.stormGlassAPISource],
+            time: point.time,
+            waveDirection: point.waveDirection[this.stormGlassAPISource],
+            waveHeight: point.waveHeight[this.stormGlassAPISource],
+            windDirection: point.windDirection[this.stormGlassAPISource],
+            windSpeed: point.windSpeed[this.stormGlassAPISource],
+        }))
+    }
+
+    // Partial makes all properties of the type to be optional
+    private isValidPoint(point: Partial<StormGlassPoint>): boolean {
+        // !! forces the return to be 
+        // ?. - optional changing - if exists
+
+        return !!(
+            point.time &&
+            point.swellDirection?.[this.stormGlassAPISource] &&
+            point.swellHeight?.[this.stormGlassAPISource] &&
+            point.swellPeriod?.[this.stormGlassAPISource] &&
+            point.waveDirection?.[this.stormGlassAPISource] &&
+            point.waveHeight?.[this.stormGlassAPISource] &&
+            point.windDirection?.[this.stormGlassAPISource] &&
+            point.windSpeed?.[this.stormGlassAPISource]
+        )
     }
 }
 
